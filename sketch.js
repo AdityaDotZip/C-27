85150246f9818f52f77fa6e580a0e67502e2355f
AdityaDotZip <ap2006111@gmail.com>
2020-08-28 15:15:37 +0530

    chains

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, log6, chain1, chain2, chain3;
 var backgroundImg,platform;
 
 function preload() {
@@ -33,8 +33,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(200, 150, 150, PI/2);
 
     bird = new Bird(100,100);
+
+    chain1 = new Chain(bird.body, log6.body);
 
 }
 
@@ -58,7 +61,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+
+    chain1.display();
 }
